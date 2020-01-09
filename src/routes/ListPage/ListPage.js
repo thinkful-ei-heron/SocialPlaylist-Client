2a61585b3679edc6b4bcfc6b29caed4cb1383da0
julio hernandez <hernandezjulio58@ymail.com>
2020-01-09 11:11:45 -0800

    css stuff

--- src/routes/ListPage/ListPage.js
+++ src/routes/ListPage/ListPage.js
@@ -1,63 +1,62 @@
-import React, { Component } from "react";
-import ListsApiService from "../../services/lists-api-service";
-import PlayListContext from "../../contexts/PlayListContext";
-import Spot from "../../components/Spot/Spot";
-import Map from "../../components/Map/Map";
+import React, { Component } from 'react';
+import ListsApiService from '../../services/lists-api-service';
+import PlayListContext from '../../contexts/PlayListContext';
+import Spot from '../../components/Spot/Spot';
+import Map from '../../components/Map/Map';
 
 export class ListPage extends Component {
   static contextType = PlayListContext;
 
   spotsFromServer = {
-    list_name: "list name",
+    list_name: 'list name',
     list_id: 1,
-    tags: "#sick #cheap",
-    created_by: "username",
+    tags: '#sick #cheap',
+    created_by: 'username',
     liked: 10,
     tried: 100,
     spots: [
       {
         id: 1,
-        name: "spots name",
-        tags: "#bestdrinks #goodmusic",
-        address: "361 fake st.",
-        city: "city name",
-        state: "ST",
+        name: 'spots name',
+        tags: '#bestdrinks #goodmusic',
+        address: '361 fake st.',
+        city: 'city name',
+        state: 'ST',
         lat: 12.091823,
         lng: 31.31525
       },
       {
         id: 1,
-        name: "spots name",
-        tags: "#bestdrinks #goodmusic",
-        address: "361 fake st.",
-        city: "city name",
-        state: "ST",
+        name: 'spots name',
+        tags: '#bestdrinks #goodmusic',
+        address: '361 fake st.',
+        city: 'city name',
+        state: 'ST',
         lat: 12.091823,
         lng: 31.31525
       },
       {
         id: 1,
-        name: "spots name",
-        tags: "#bestdrinks #goodmusic",
-        address: "361 fake st.",
-        city: "city name",
-        state: "ST",
+        name: 'spots name',
+        tags: '#bestdrinks #goodmusic',
+        address: '361 fake st.',
+        city: 'city name',
+        state: 'ST',
         lat: 12.091823,
         lng: 31.31525
       },
       {
         id: 1,
-        name: "home",
-        tags: "#bestdrinks #goodmusic",
-        address: "361 fake st.",
-        city: "city name",
-        state: "ST",
+        name: 'home',
+        tags: '#bestdrinks #goodmusic',
+        address: '361 fake st.',
+        city: 'city name',
+        state: 'ST',
         lat: 34.001522,
         lng: -118.437215
       }
     ]
   };
-
 
   renderSpot = () => {
     return this.spotsFromServer.spots.map(spot => (
@@ -85,7 +84,7 @@ export class ListPage extends Component {
   render() {
     return (
       <div>
-        <h1>My List</h1>
+        <h2>My List</h2>
         {/* After API is set change these to props or context */}
         <Map spots={this.spotsFromServer.spots} />
         {this.renderSpot(this.spotsFromServer)}
