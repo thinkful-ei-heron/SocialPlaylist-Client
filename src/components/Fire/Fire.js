f74cd8ad20fd4d964f1bdfca96fe358c02254c68
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-17 11:29:08 -0800

    added animation for fire

--- src/components/Fire/Fire.js
+++ src/components/Fire/Fire.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Fire.css";
 import fire from "./fire.png"
+import fireAnimation from "./fire.gif"
 
 const Fire = (props) => {
   return (
     <div className="fire-container">
       <div>
-        <img src={fire} className="fire"/>
+        <img src={fireAnimation} className="fire"/>
         <p className='firetext'>HOT LIST!</p>
       </div>
     </div>
